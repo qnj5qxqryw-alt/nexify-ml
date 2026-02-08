0ac58355a1a8917fb5dd68e71a1ca2350517c130
unknown <mansoorssonko27@gmail.com>
2026-02-08 13:38:32 -0800

    Fix port syntax

--- ml/app.py
+++ ml/app.py
@@ -8,5 +8,5 @@ def home():
     return jsonify({"status": "ML service running"})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT, 5000"))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
